08d8f2b2a472ea1639b800efa689d75691dceb8e
ramurn <uname.raja08@gmail.com>
2018-08-16 19:01:05 +0000

    master:ramurn:chefNodeAWS.py script clean state

--- Lab/chefnodeAWS.py
+++ Lab/chefnodeAWS.py
@@ -1,5 +1,5 @@
+
 def ipVal(obj):
-    print ("Validating IP Address..")
     while True:
             count = 0
             for i in obj.split("."):
@@ -21,5 +21,35 @@ wpub=input("Enter ChefWS Public IP: ")
 wpub=ipVal(wpub)
 wpvt=input("Enter ChefWS Pvt IP: ")
 wpvt=ipVal(wpvt)
+
+def setPubVal (var):
+    varval=var+"           ec2-"+str(var.split(".")[0])+"-"+str(var.split(".")[1])+"-"+str(var.split(".")[2])+"-"+str(var.split(".")[3])+".us-east-2.compute.amazonaws.com"
+    return varval
+
+def setPvtVal (var):
+    varval=var+"           ip-"+str(var.split(".")[0])+"-"+str(var.split(".")[1])+"-"+str(var.split(".")[2])+"-"+str(var.split(".")[3])+".us-east-2.compute.amazonaws.com"
+    return varval
+
+
+mpubVal=setPubVal(mpub)
+wpubVal=setPubVal(wpub)
+mpvtVal=setPvtVal(mpvt)
+wpvtVal=setPvtVal(wpvt)
+
+
+
+with open ('/etc/hosts','a') as hf:
+    hf.write("\n")
+    hf.write(mpubVal)
+    hf.write("\n")
+    hf.write(mpvtVal)
+    hf.write("\n")
+    hf.write(wpubVal)
+    hf.write("\n")
+    hf.write(wpvtVal)
+    hf.write("\n")
+
+
+
 
 
